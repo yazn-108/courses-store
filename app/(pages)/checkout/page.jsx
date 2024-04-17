f3c0036b725a5feca5c45410b8ae80e225e27d31
Ezzedine2000 <gamesalab22@gmail.com>
2024-04-17 06:33:02 +0300

    Courses store

--- app/(pages)/checkout/page.jsx
+++ app/(pages)/checkout/page.jsx
@@ -1,21 +1,23 @@
 "use client"
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './_components/CheckoutForm';
 import { useSearchParams } from 'next/navigation';
-const Page = async () => {
-	const SearchParams = await useSearchParams()
-	const stripePromise = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+const Page = () => {
+	const SearchParams = useSearchParams()
+	const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 	const options = {
 		mode: 'payment',
 		currency: "usd",
 		amount: SearchParams.get('amount') * 100,
 	};
 	return (
-		<Elements stripe={stripePromise} options={options}>
-			<CheckoutForm amount={Number(SearchParams.get("amount"))} />
-		</Elements>
+		<Suspense fallback={<div>Loading...</div>}>
+			<Elements stripe={stripePromise} options={options}>
+				<CheckoutForm amount={Number(SearchParams.get("amount"))} />
+			</Elements>
+		</Suspense>
 	)
 }
 export default Page
