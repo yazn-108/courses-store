027c23044e206c20b8f9ef2550d088c02b6ea9e3
Ezzedine2000 <gamesalab22@gmail.com>
2024-04-17 09:48:02 +0300

    Courses store

---
+++ app/api/allProducts/[productsId]/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import connect from "@/lib/db";
+import AllProducts from "@/lib/modals/AllProducts"
+export const GET = async (req, { params }) => {
+  const adminId = req.headers.get("authorization")?.split(" ")[1]
+  const productId = params?.productsId;
+  try {
+    if (adminId === process.env.NEXT_PUBLIC_ADMIN_ID) {
+      await connect();
+      const product = await AllProducts.findOne({ productId });
+      if (!product) {
+        return new NextResponse(
+          JSON.stringify({
+            message: "product not found or does not belong to the user",
+          }),
+          { status: 404 }
+        );
+      }
+      return new NextResponse(JSON.stringify(product), { status: 200 });
+    }
+    return new NextResponse("you don't have access to api", { status: 403 })
+  } catch (error) {
+    return new NextResponse("Error in fetching product " + error, {
+      status: 500,
+    });
+  }
+};
---
+++ app/api/filter/checkUserProduct/route.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import connect from "@/lib/db";
+import Product from "@/lib/modals/CartProduct"
+export const POST = async (req) => {
+  const adminId = req.headers.get("authorization")?.split(" ")[1]
+  try {
+    if (adminId === process.env.NEXT_PUBLIC_ADMIN_ID) {
+      const body = await req.json();
+      const { productId, userId } = body;
+      await connect();
+      const product = await Product.findOne({ productId, user: userId })
+      return new NextResponse(JSON.stringify(product), { status: 200 });
+    }
+    return new NextResponse("you don't have access to api", { status: 403 })
+  } catch (error) {
+    return new NextResponse("Error in fetching product " + error, {
+      status: 500,
+    });
+  }
+};
