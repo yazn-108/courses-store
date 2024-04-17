a384b7a71debbb47aeebe715b51f5cc9fa4cfc2c
Ezzedine2000 <gamesalab22@gmail.com>
2024-04-17 06:38:36 +0300

    Courses store

--- app/api/Cart/deleteAll/route.js
+++ app/api/Cart/deleteAll/route.js
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 export const DELETE = async (req) => {
   const adminId = req.headers.get("AdminToken")
   try {
-    if (adminId === process.env.NEXT_PUBLIC_ADMIN_ID) {
-      const userId = req.headers.get("authorization")?.split(" ")[1]
-      await connect();
-      await Product?.deleteMany({ user: { $in: userId } })
-      return new NextResponse("deleted all cart products", { status: 200 })
-    }
-    return new NextResponse("you don't have access to api", { status: 403 })
+    // if (adminId === process.env.NEXT_PUBLIC_ADMIN_ID) {
+    const userId = req.headers.get("authorization")?.split(" ")[1]
+    await connect();
+    await Product?.deleteMany({ user: { $in: userId } })
+    return new NextResponse("deleted all cart products", { status: 200 })
+    // }
+    // return new NextResponse("you don't have access to api", { status: 403 })
   } catch (error) {
     return new NextResponse("Error in fetching users" + error, { status: 500 })
   }
--- app/api/filter/checkUserProduct/route.js
+++ app/api/filter/checkUserProduct/route.js
@@ -4,14 +4,14 @@ import Product from "@/lib/modals/CartProduct"
 export const POST = async (req) => {
   const adminId = req.headers.get("authorization")?.split(" ")[1]
   try {
-    if (adminId === process.env.NEXT_PUBLIC_ADMIN_ID) {
-      const body = await req.json();
-      const { productId, userId } = body;
-      await connect();
-      const product = await Product.findOne({ productId, user: userId })
-      return new NextResponse(JSON.stringify(product), { status: 200 });
-    }
-    return new NextResponse("you don't have access to api", { status: 403 })
+    // if (adminId === process.env.NEXT_PUBLIC_ADMIN_ID) {
+    const body = await req.json();
+    const { productId, userId } = body;
+    await connect();
+    const product = await Product.findOne({ productId, user: userId })
+    return new NextResponse(JSON.stringify(product), { status: 200 });
+    // }
+    // return new NextResponse("you don't have access to api", { status: 403 })
   } catch (error) {
     return new NextResponse("Error in fetching product " + error, {
       status: 500,
