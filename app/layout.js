a6c6ff76a64736295f2e59a994009e6fe9294c5d
Ezzedine2000 <gamesalab22@gmail.com>
2024-04-19 12:19:32 +0300

    Courses store

--- app/layout.js
+++ app/layout.js
@@ -16,8 +16,20 @@ export default function RootLayout({ children }) {
       <ClerkProvider>
         <html lang="en">
           <head>
-            <title>Courses store</title>
             <link rel="icon" href="/favicon.svg" type="image/svg" sizes="any" />
+            <title>Courses store</title>
+            <meta name="description" content="course store is a full stack next.js project." />
+            <meta property="og:url" content="https://yazn-courses-store.vercel.app/" />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content="Courses store" />
+            <meta property="og:description" content="course store is a full stack next.js project." />
+            <meta property="og:image" content="https://raw.githubusercontent.com/yazn-108/courses-store/main/public/courses-store-image.png" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta property="twitter:domain" content="yazn-courses-store.vercel.app" />
+            <meta property="twitter:url" content="https://yazn-courses-store.vercel.app/" />
+            <meta name="twitter:title" content="Courses store" />
+            <meta name="twitter:description" content="course store is a full stack next.js project." />
+            <meta name="twitter:image" content="https://raw.githubusercontent.com/yazn-108/courses-store/main/public/courses-store-image.png" />
           </head>
           <body className={MainFont.className}>
             <Header />
